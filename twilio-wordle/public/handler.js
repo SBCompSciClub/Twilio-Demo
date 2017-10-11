d7c23a993d029cd51a65109178204093c162a208
Shivan Modha <shivanmodha@gmail.com>
2017-10-11 17:30:14 -0400

    Send Test

--- twilio-wordle/public/handler.js
+++
@@ -1,4 +0,0 @@
-let canvas = document.getElementById("wordle-container");
-let style = window.getComputedStyle(canvas);
-canvas.width = parseInt(style.width);
-canvas.height = parseInt(style.height);
